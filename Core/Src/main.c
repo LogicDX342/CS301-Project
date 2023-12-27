edc371df5c637e30dbbdfa31408fab77e3b062a1
LogicDX342 <52480482+LogicDX342@users.noreply.github.com>
2023-12-27 23:06:25 +0800

    Add NRF24L01 driver

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -30,6 +30,7 @@
 #include "touch.h"
 #include "remote.h"
 #include "24cxx.h"
+#include "24l01.h"
 #include "ff.h"
 #include "lv_conf.h"
 #include "lv_port_disp.h"
@@ -70,6 +71,9 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 lv_ui guider_ui;
+u8 key, mode;
+u8 tmp_buf[33]; 
+u16 t=0;
 /* USER CODE END 0 */
 
 /**
@@ -106,7 +110,8 @@ int main(void)
   MX_TIM5_Init();
   MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
-  Remote_Init(); 
+  NRF24L01_Init(); 
+  Remote_Init();
   LCD_Init();
   tp_dev.init();
   HAL_TIM_Base_Start_IT(&htim6);
@@ -116,12 +121,83 @@ int main(void)
   lv_port_disp_init();
   lv_port_indev_init();
   setup_ui(&guider_ui);
+
+  // NRF24L01_Init();   // 初始化NRF24L01
+  // POINT_COLOR = RED; // 设置字体为红色
+  // LCD_ShowString(60, 50, 200, 16, 16, "Mini STM32");
+  // LCD_ShowString(60, 70, 200, 16, 16, "NRF24L01 TEST");
+  // LCD_ShowString(60, 90, 200, 16, 16, "ATOM@ALIENTEK");
+  // LCD_ShowString(60, 110, 200, 16, 16, "2014/3/12");
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    // while (NRF24L01_Check()) // 检查NRF24L01是否在位.
+    // {
+    //   LCD_ShowString(60, 130, 200, 16, 16, "NRF24L01 Error");
+    //   HAL_Delay(200);
+    //   LCD_Fill(60, 130, 239, 130 + 16, WHITE);
+    //   HAL_Delay(200);
+    // }
+    // LCD_ShowString(60, 130, 200, 16, 16, "NRF24L01 OK");
+    // mode=0;
+    // LCD_Fill(10, 150, 240, 166, WHITE); // 清空上面的显示
+    // POINT_COLOR = BLUE;                 // 设置字体为蓝色
+    // if (mode == 0)                      // RX模式
+    // {
+    //   LCD_ShowString(60, 150, 200, 16, 16, "NRF24L01 RX_Mode");
+    //   LCD_ShowString(60, 170, 200, 16, 16, "Received DATA:");
+    //   NRF24L01_RX_Mode();
+    //   while (1)
+    //   {
+    //     if (NRF24L01_RxPacket(tmp_buf) == 0) // 一旦接收到信息,则显示出来.
+    //     {
+    //       tmp_buf[32] = 0; // 加入字符串结束符
+    //       LCD_ShowString(0, 190, 239, 32, 16, tmp_buf);
+    //     }
+    //     else
+    //       delay_us(100);
+    //     t++;
+    //     if (t == 10000) // 大约1s钟改变一次状态
+    //     {
+    //       t = 0;
+    //     }
+    //   };
+    // }
+    // else // TX模式
+    // {
+    //   LCD_ShowString(60, 150, 200, 16, 16, "NRF24L01 TX_Mode");
+    //   NRF24L01_TX_Mode();
+    //   mode = ' '; // 从空格键开始
+    //   while (1)
+    //   {
+    //     if (NRF24L01_TxPacket(tmp_buf) == TX_OK)
+    //     {
+    //       LCD_ShowString(60, 170, 239, 32, 16, "Sended DATA:");
+    //       LCD_ShowString(0, 190, 239, 32, 16, tmp_buf);
+    //       key = mode;
+    //       for (t = 0; t < 32; t++)
+    //       {
+    //         key++;
+    //         if (key > ('~'))
+    //           key = ' ';
+    //         tmp_buf[t] = key;
+    //       }
+    //       mode++;
+    //       if (mode > '~')
+    //         mode = ' ';
+    //       tmp_buf[32] = 0; // 加入结束符
+    //     }
+    //     else
+    //     {
+    //       LCD_ShowString(60, 170, 239, 32, 16, "Send Failed ");
+    //       LCD_Fill(0, 188, 240, 218, WHITE); // 清空上面的显示
+    //     };
+    //     HAL_Delay(1500);
+    //   };
+    // }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
